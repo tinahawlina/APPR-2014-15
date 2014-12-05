01ca77b31c740b8a83ed4d56d8f9c8741520b8da
Tina Hawlina <tina.hawlina@gmail.com>
2014-12-05 18:50:06 +0100

    moskihrbtno, drugač

--- slike/moskihrbtno.R
+++ slike/moskihrbtno.R
@@ -1,4 +1,4 @@
 pdf("slike/moskihrbtno.pdf") #naslov pdfja
-pie(table(moskihrbtno[,"Ime"]), main = "Moški hrbtno", cex.main = 1.5)
-#text(0,1,"Moški hrbtno", cex =2)
+pie(table(moskihrbtno[,"Ime"]), main = "Moški hrbtno", cex.main = 1.5, cex=0.8)
+#text(0,1,"MOški hrbtno", cex =2)
 dev.off()

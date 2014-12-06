8f55b405a9a2fdae79e089095e6c37c667cc06a1
Tina Hawlina <tina.hawlina@gmail.com>
2014-12-06 21:02:38 +0100

    popravljena grafa, prikaz pdf, v poročilu

--- slike/zenskeprosto.R
+++ slike/zenskeprosto.R
@@ -1,2 +1,5 @@
+pdf("slike/zenskeprosto.pdf")
 plot(zenskeprosto[,2], main="Ženske prosto: najuspešnejše rekorderke",
-     ylab="število rekordov",col="palevioletred1",las=2, cex.names=0.6)
+     ylab="število rekordov",col="palevioletred1",las=2, cex.names=0.5)
+
+dev.off()

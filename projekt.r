26c10285f099ed64f6f6a223c26b01ff302aa93a
Tina Hawlina <tina.hawlina@gmail.com>
2015-01-15 14:17:26 +0100

    popravljeno kodiranje UTF-8 zemljevidi

--- projekt.r
+++ projekt.r
@@ -14,7 +14,8 @@ source("slike/zenskeprosto.R", encoding="UTF-8")
 
 
 # 3. faza: Analiza in vizualizacija podatkov
-source("vizualizacija/vizualizacija.r")
+source("vizualizacija/vizualizacija.r", encoding ="UTF-8")
+
 
 # 4. faza: Napredna analiza podatkov
 #source("analiza/analiza.r")

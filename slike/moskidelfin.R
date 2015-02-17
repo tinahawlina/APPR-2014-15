5bf4b0a2c80bf943e02382f9a5d086497ae3158f
Tina Hawlina <tina.hawlina@gmail.com>
2015-02-17 16:05:54 +0100

    popravila črke, pri sliki moškidelfin države slovensko

--- slike/moskidelfin.R
+++ slike/moskidelfin.R
@@ -1,9 +1,17 @@
+
 moskidelfin$Drzava.prvenstva <- gsub(".*, ", "", moskidelfin$Kraj.prvenstva)
-moskidelfin$Drzava.prvenstva[grep("Germany", moskidelfin$Drzava.prvenstva)] <- "Germany"
-moskidelfin$Drzava.prvenstva[grep("USSR", moskidelfin$Drzava.prvenstva)] <- "Russia"
-moskidelfin$Drzava.prvenstva[grep("USA", moskidelfin$Drzava.prvenstva)] <- "United States"
+moskidelfin$Drzava.prvenstva[grep("Germany", moskidelfin$Drzava.prvenstva)] <- "Nemčija"
+moskidelfin$Drzava.prvenstva[grep("United States", moskidelfin$Drzava.prvenstva)] <- "Združene države Amerike"
+moskidelfin$Drzava.prvenstva[grep("Italy", moskidelfin$Drzava.prvenstva)] <- "Italija"
+moskidelfin$Drzava.prvenstva[grep("Japan", moskidelfin$Drzava.prvenstva)] <- "Japonska"
+moskidelfin$Drzava.prvenstva[grep("Brazil", moskidelfin$Drzava.prvenstva)] <- "Brazilija"
+moskidelfin$Drzava.prvenstva[grep("Austria", moskidelfin$Drzava.prvenstva)] <- "Avstrija"
+moskidelfin$Drzava.prvenstva[grep("Australia", moskidelfin$Drzava.prvenstva)] <- "Avstralija"
+moskidelfin$Drzava.prvenstva[grep("Canada", moskidelfin$Drzava.prvenstva)] <- "Kanada"
+moskidelfin$Drzava.prvenstva[grep("Hungary", moskidelfin$Drzava.prvenstva)] <- "Madžarska"
+moskidelfin$Drzava.prvenstva[grep("Spain", moskidelfin$Drzava.prvenstva)] <- "Španija"
 rekordi <- table(moskidelfin$Drzava.prvenstva)
 pdf("slike/moskidelfin.pdf") #naslov pdfja
-pie(rekordi, cex = 0.5, main = "Moški delfin: Države doseženih rekordov", col=rainbow(13))
+pie(rekordi, cex = 0.7, main = "Moški delfin: Države doseženih rekordov", col=rainbow(13))
 
 dev.off()
--- slike/moskihrbtno.R
+++ slike/moskihrbtno.R
@@ -1,4 +1,4 @@
 pdf("slike/moskihrbtno.pdf") #naslov pdfja
-pie(table(moskihrbtno[,"Ime"]), main = "Moški hrbtno", cex.main = 1.5)
+pie(table(moskihrbtno[,"Ime"]), main = "Moški hrbtno", cex.main = 1.5, cex=0.7)
 #text(0,1,"Moški hrbtno", cex =2)
 dev.off()
